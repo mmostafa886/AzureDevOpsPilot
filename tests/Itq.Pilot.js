59ffe674558b31dc49f613851e21cd2d58b7280a
mmostafa886 <36827060+mmostafa886@users.noreply.github.com>
2020-08-30 12:50:18 +0200

    Updating Script

--- tests/Itq.Pilot.js
+++ tests/Itq.Pilot.js
@@ -3,24 +3,24 @@ import { Selector, t } from 'testcafe';
 const uNameField = '#login';
 const pWordField = '#password';
 const lgnBtn = Selector('button[type=submit]');
-
 const pageTitle = Selector('[data-menu-xmlid="itq_basic.itq_system_menu_main"]').nth(0);
-
-//const usrTitle = Selector('[class="oe_topbar_name"]');
 const usrTitle = Selector('.oe_topbar_name');
+const selectDB = Selector('a').withText('sprint_db')
 
 
 fixture `Odoo Pilot`
-    .page `http://40.65.99.236:49994/`;
+    .page `http://40.65.99.236:49004/`;
 
 test('Login', async t => {
     
+    await t .click(selectDB);
+
     await t
         .setTestSpeed (0.8)
         .typeText(uNameField, 'admin',{paste:true})
         .typeText(pWordField, 'admin',{paste:true})
         .click(lgnBtn)
-        .expect(usrTitle.innerText).contains('Administrator')
+      //  .expect(usrTitle.innerText).contains('Administrator')
         .expect(pageTitle.innerText).contains('الأنظمة المالية والأدارية');
       console.log(await pageTitle.textContent);
       console.log(await usrTitle.textContent);
